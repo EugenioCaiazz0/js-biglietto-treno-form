a34777efa9f2b6ec60647d2bf8ac8f7f710e6e03
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-02-21 18:38:36 +0100

    Fix dumb error, add functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,57 @@
 
-const kilometriViaggio = parseInt(prompt("Inserire lunghezza tragitto"));
+/*const kilometriViaggio = parseInt(prompt("Inserire lunghezza tragitto"));
 
 console.log(kilometriViaggio);
 
 const etaPasseggero = parseInt(prompt("Scrivere la propria età"));
 
-console.log(etaPasseggero);
+console.log(etaPasseggero);*/
 
+let passengerID;
+let travelDistance;
 const costoKm = 0.21;
-
 let costoViaggio = 1;
+let etaPasseggero;
 
-if (etaPasseggero < 19) {
+function getID() {
+    const inputValue = document.getElementById('nameSurnamePassenger').value;
+    passengerID = inputValue;
+    console.log(passengerID);
+}
+
+function getDistance() {
+    const inputValue = document.getElementById('travelDistance').value;
+    travelDistance = inputValue;
+    console.log(travelDistance);
+}
+
+function youngPassengerF() {
+    etaPasseggero = 1;
+}
+
+function averagePassengerF() {
+    etaPasseggero = 2;
+}
+
+function oldPassengerF() {
+    etaPasseggero = 3;
+}
+
+if (etaPasseggero == 1) {
     costoViaggio = kilometriViaggio * 0.21 * 0.80;
 } 
-else if (etaPasseggero > 65) {
-    costoViaggio = kilometriViaggio * 0.21 * 0.60;
-}
-else {
+else if (etaPasseggero == 2) {
     costoViaggio = kilometriViaggio * 0.21;
+}
+else if(etaPasseggero == 3) {
+    costoViaggio = kilometriViaggio * 0.21 * 0.60;
 }
 
 if (Number.isNaN(costoViaggio)) {
     alert("Errore con l'inserimento dei dati");
 }
 
-/*CALCOLATO COSTO VIAGGIO*/
-
 const costoViaggioFixed = costoViaggio.toFixed(2);
 
-
-const btn1 = document.querySelector('.btn1');
-
-btn1.addEventListener('click',function(){
-    console.log(costoViaggio);
-  })
+console.log(costoViaggioFixed)
+/*CALCOLATO COSTO VIAGGIO*/
